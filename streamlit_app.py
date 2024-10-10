a170905933323f0c74ddde367e4782800cf41b34
James <jimbo.fox@hotmail.co.uk>
2024-10-10 11:01:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,73 @@
+import pandas as pd
+from pandas.io.formats.style_render import Subset
 import streamlit as st
+from relativeImp import relativeImp
+import statsmodels.api as sm
 
-st.title('🎈 App Name')
+def is_sig(val):
+    color = 'green' if val <= 0.05 else 'red'
+    return f'background-color: {color}; color: white;'
 
-st.write('Hello world!')
+uploaded_file = st.file_uploader('Choose a file')
+
+if uploaded_file is not None:
+   df=pd.read_excel(uploaded_file)
+
+   with st.expander('Click to See Input Data'):
+       st.write(df)
+
+   st.subheader('Please select the dependent variable:')
+   dependent = st.selectbox('Select one variable:', df.columns)
+
+   st.subheader('Please select the independent variables:')
+   independents = st.multiselect('Select the independent variables:', df.columns)
+
+   main_body = st.form('main_body')
+   submit = main_body.form_submit_button("Calculate")
+
+   if submit:
+
+       yName = dependent
+       xNames = independents
+       full_list = [yName] + xNames
+
+       correls = df[full_list].corr()[yName].tolist()[1:]
+
+       model = sm.OLS(df[yName], sm.add_constant(df[xNames]))
+       model_fit = model.fit()
+       coeffs = model_fit.params.tolist()[1:]
+       pvalues = model_fit.pvalues.tolist()[1:]
+
+       df_results = relativeImp(df, outcomeName = yName, driverNames = xNames)
+       r_square = df_results['rawRelaImpt'].sum()       
+
+       df_results.insert(1, 'pvalue', pvalues)
+       df_results.insert(1, 'coeff', coeffs)
+       df_results.insert(1, 'correls', correls)
+
+       st.title('Driver Analysis Results')       
+       st.subheader('Results')
+       st.write(f"R Square: {r_square:.2f}")
+
+
+
+       st.dataframe(
+           df_results.style.applymap(is_sig, subset=['pvalue']),
+           column_config={
+               "driver": "Variable",
+               "correls": st.column_config.NumberColumn("Correlation",
+                                                            format="%.3f"),
+               "coeff": st.column_config.NumberColumn("Coefficient",
+                                                            format="%.3f"),
+               "pvalue": st.column_config.NumberColumn("P-Value",
+                                                            format="%.3f"),
+               "rawRelaImpt": st.column_config.NumberColumn("Raw Relative Importance",
+                                                            format="%.3f"),
+               "normRelaImpt": st.column_config.NumberColumn("Standardized Relative Importance",
+                                                            format="%.3f"),
+               },
+           hide_index=True,
+           )
+
+else:
+    st.warning('you need to upload an excel data file to begin')
